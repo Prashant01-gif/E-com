c5cd71a6681b00a7d2f83f6f509f415a73847d2c
Prashant01-gif <imprashantsenn@gmail.com>
2025-07-11 07:10:52 +0545

    gandu

--- src/Component/Model.jsx
+++ src/Component/Model.jsx
@@ -4,8 +4,14 @@ function Model({ item, onClose }) {
   if (!item) return null;
 
   return (
-    <div className="fixed inset-0 bg-gray-200 flex items-center justify-center z-50">
-      <div className="bg-white rounded-2xl w-[90%] max-w-3xl h-[75%] flex ">
+    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50"
+     onClick={onClose}
+    >
+      <div className="bg-white rounded-2xl w-[90%] max-w-3xl h-[75%] flex "
+      onClick={(e)=>{
+        e.stopPropagation()
+      }}
+      >
         <div className="md:w-1/2 w-full h-64 md:h-full">
           <img
             src={item.image}
@@ -20,13 +26,12 @@ function Model({ item, onClose }) {
               {item.name}
             </h2>
             <p className="text-gray-600 mt-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-              nec odio.
+              {item.instructions}
             </p>
           </div>
 
           <button
-            onClick={onClose}
+           
             className="mt-6 md:mt-auto bg-gray-800 text-white px-6 py-3 rounded-lg hover:bg-gray-700 transition duration-300"
           >
             Close
