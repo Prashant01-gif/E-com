ceb14816da17e3ab8bfd8e6c2d9be195179cc38b
Prashant01-gif <imprashantsenn@gmail.com>
2025-07-18 21:09:41 +0545

    new

--- src/Component/Cart/Cart.jsx
+++ src/Component/Cart/Cart.jsx
@@ -13,25 +13,27 @@ const Cart = () => {
   return (
     <div>
       <Navbar />
+
       <div>
         {cart.map((item, index) => (
           <Cartcart item={item} setCart={setCart} key={index} />
         ))}
       </div>
-      <div className="flex justify-around ml-50">
+
+      <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mt-4">
         <div className="flex gap-2 text-lg items-center">
-          <div className="text-orange-500 text-lg font-bold">Total Amount: </div>
-          <div className="text-black font-bold text-lg sm:text-xl"> ${Totalamount(cart)}</div>
+          <div className="text-orange-500 text-lg font-bold">Total Amount:</div>
+          <div className="text-black font-bold text-lg sm:text-xl">
+            ${Totalamount(cart)}
+          </div>
         </div>
 
         <div>
           <Button onClick={() => setShowModal(true)} title="Check out" />
         </div>
-        <div>
-          
-        </div>
       </div>
-       <CheckOutModal visible={showModal} setVisible={setShowModal} />
+
+      <CheckOutModal visible={showModal} setVisible={setShowModal} />
     </div>
   );
 };
